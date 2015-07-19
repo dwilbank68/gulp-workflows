b412b8a36ee80b7017fffe04a7b0020507d07a6d
David Wilbanks <dwilbank@gmail.com>
2015-07-19 02:03:48 -0700

    added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
     'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
@@ -53,6 +55,18 @@ gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(jsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass',  'connect', 'watch']); // runs as just 'gulp'
+gulp.task('html', function () {
+    gulp.src(htmlSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('json', function () {
+    gulp.src(jsonSources)
+        .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass',  'connect', 'watch']); // runs as just 'gulp'
